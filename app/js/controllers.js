859d94756dc4d50b5fb78231b9983b10e3d3b164
Lunchmeat <shangjincg@gmail.com>
2014-04-15 16:55:07 -0700

    Initial commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,50 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('TechSkillController', ['$scope', '$http', function($scope, $http) {
+
+	$http.get('techSkills/techSkills.json').success(function(data1) {
+	  $scope.techSkills = data1;
+	});
+
+	$http.get('techSkills/techTypes.json').success(function(data) {
+	  $scope.techTypes = data;
+	  $scope.skillquery = "";
+	});
+	
+	$scope.setSkillQuery = function(query) {
+      $scope.skillquery = query;
+    }
 
   }])
+
+  .controller('ProjectExperienceController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+
+	$http.get('projectExperiences/' + $routeParams.pid + '.json').success(function(data2) {  
+	  $scope.projects = data2;	  
+	});
+
+	$http.get('projectExperiences/projectList.json').success(function(data2) {  
+	  $scope.projectList = data2;	  
+	});
+  }])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   .controller('MyCtrl2', [function() {
 
   }]);
+
+
